49325a6429e7ddfc8c4aa29d7c90fcccc5b009ac
Tom Rasmussen <torasmu@amazon.com>
2020-04-02 11:50:55 -0500

    testing airtable api

--- controller.js
+++ controller.js
@@ -1,3 +1,35 @@
-netlifyIdentity.on("login", user =>
-  console.log("login", netlifyIdentity.currentUser())
-);
+var Airtable = require("airtable");
+var key = "key4NpGO6wBYyX3Ca";
+var base = new Airtable({ apiKey: key }).base("appVCgPdvws9nVFzV");
+
+netlifyIdentity.on("login", user => {
+  console.log("login", user);
+  // Try to get the airtable api
+  base("Table 1")
+    .select({
+      // Selecting the first 3 records in Grid view:
+      maxRecords: 3,
+      view: "Grid view"
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+
+        records.forEach(function(record) {
+          console.log("Retrieved", record.get("email"));
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+      },
+      function done(err) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      }
+    );
+  location.href = "signup.html";
+});
