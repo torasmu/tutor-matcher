dc2b7383111dbeefd7cbdb7dbb40b1397bc5241d
Tom Rasmussen <torasmu@amazon.com>
2020-04-06 17:31:09 -0500

    it now redirects for new users

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 netlifyIdentity.on("login", user => {
-  console.log("login", user);
-  // var id = user.get("id");
+  // Report user that logged in
+  console.log("current user is ", user);
+  console.log("current user id is ", user.id);
   // Try to get the airtable api
   fetch(window.location.origin + "/.netlify/functions/getAllUserIds")
     .then(response => {
       return response.json();
     })
-    .then(data => {
-      console.log(data);
+    .then(x => {
+      // Convert JSON string to array of IDs
+      console.log("type of x: ", typeof x);
+      console.log("x: ", x);
+
+      var userHasAlreadyRegistered = x.includes(user.id);
+
+      if (userHasAlreadyRegistered) {
+        console.log("already registered");
+      } else {
+        console.log("forwarding to signup page");
+        location.href = "signup.html";
+      }
+
+      return;
     });
-  // location.href = "signup.html";
 });
